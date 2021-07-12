c21b46060e1b88804fb94a871868f4dcb524fd9a
Ashitosh Ghate <61339195+ashitoshghate@users.noreply.github.com>
2021-07-12 16:04:49 +0530

    basic.c

---
+++ basic.c
@@ -0,0 +1,14 @@
+/*git hub Quetion get input integer from user 
+and twice it and print string as it is 
+*/
+#include<stdio.h>
+int main(){
+	int N;
+	char S[100];
+	scanf("%d\n",&N);
+	scanf("%s\n",&S);
+	printf("%d\n",N*2);
+	printf("%s",S);
+	
+	return 0;
+}
